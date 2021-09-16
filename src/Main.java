4a33fbd7d56be67e590bf8dbc842f8803dc5d534
settler <adnanvahora144@gmail.com>
2021-09-16 09:54:53 +0530

    add author information

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
+    /**
+     * @since v1.0 16/9/2021
+     * @author  20CE155 Adnan Vahora
+     * @param args CLI args
+     */
     public static void main(String[] args) {
+        System.out.println("This program is prepared by  20CE155 ADNAN VAHORA\n" +
+                "Total line of code = <<value>>\n" +
+                "Total lines for comment= <<value>>\n");
 
     }
 }
